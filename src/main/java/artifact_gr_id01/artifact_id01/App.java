87a1b35c098d06a593b047c9c3b8ec33bc5d769e
Adi Adnan <adi.adnan15@yahoo.com>
2017-05-25 17:15:27 +0300

    Base project.

---
+++ src/main/java/artifact_gr_id01/artifact_id01/App.java
@@ -0,0 +1,13 @@
+package artifact_gr_id01.artifact_id01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/artifact_gr_id01/artifact_id01/CsvHandler.java
@@ -0,0 +1,13 @@
+package artifact_gr_id01.artifact_id01;
+
+public class CsvHandler {
+
+	CsvParserSettings settings = new CsvParserSettings();
+
+	private CsvHandler() {
+	}
+
+	public static void getRows(String filePath) {
+
+	}
+}
